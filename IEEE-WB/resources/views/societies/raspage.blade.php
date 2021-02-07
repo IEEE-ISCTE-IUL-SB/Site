e362fb580c40b5b94aeac4d14d11a140366a6c79
jmspa3 <57222312+jmspa3@users.noreply.github.com>
2021-02-07 17:36:19 +0000

    comiit

--- IEEE-WB/resources/views/societies/raspage.blade.php
+++ IEEE-WB/resources/views/societies/raspage.blade.php
@@ -2,49 +2,78 @@
 
 @section('content')
 
-<section id="section1" style="margin-top:10%;margin-bottom:20%;">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-8 mx-auto">
-                <h2>RAS</h2>
-                <p class="text-muted lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.</p>
-            </div>
+<section id="societylogosection">
+    <img src="{{asset('img/raslogo2.png')}}" class="societylogo">
+</section>
+
+<section id="missionsection">
+    <div class="row mission-row">
+        <div class="col">
+            Missão
+        </div>
+        <div class="col">
+            Missão
+        </div>
+        <div class="col">
+            Missão
         </div>
     </div>
 </section>
 
-<section id="section2" class="py-5 section-2">
-    <div class="container py-5 text-center">
-        <div class="row">
-            <div class="col-lg-8 mx-auto">
-                <h2>Demo section 2</h2>
-                <p class="text-muted lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.</p>
-            </div>
+<section id="bannersection" style="overflow:hidden;">
+    <div class="row">
+        <div class="col" style="background-color:red;">
+            <h1>jnjnjnjvn</h1>
+        </div>
+        <div class="col">
+            <img src="{{asset('img/rasSplash.jpg')}}" class="banner">
         </div>
     </div>
 </section>
 
-<section class="py-5 section-3">
-    <div class="container py-5 text-center">
-        <div class="row">
-            <div class="col-lg-8 mx-auto">
-                <h2>Demo section 3</h2>
-                <p class="text-muted lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.</p>
-            </div>
-        </div>
-    </div>
-</section>
+<style>
+    #societylogosection {
+        position: relative;
+        min-width: 40%;
+        padding-top: 10%;
+        padding-bottom: 10%;
+        overflow:hidden;
+        margin-bottom: 1%;
+    }
 
-<section class="py-5 section-4">
-    <div class="container py-5 text-center">
-        <div class="row">
-            <div class="col-lg-8 mx-auto">
-                <h2>Demo section 4</h2>
-                <p class="text-muted lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.</p>
-            </div>
-        </div>
-    </div>
-</section>
+    #missionsection {
+        margin-top: 2rem;
+        overflow: hidden;
+        margin-bottom: 2rem;
+    }
 
+    .societylogo {
+        max-width: 40%;
+        width:20%;
+        display: block;
+        margin-left:7%;
+        overflow:hidden;
+        margin-right: auto;
+    }
+
+ 
+
+    .mission-row {
+        text-align: center;
+        overflow:hidden;
+        font-size: 35px;
+        color:dimgrey;
+        overflow:hidden;
+        font-weight: bolder;
+        margin-bottom: 3%;
+    }
+
+    .banner {
+        width:120%;
+        overflow:hidden;
+
+   
+    }
+</style>
 
 @endsection
