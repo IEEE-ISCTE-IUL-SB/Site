f889a5c6bdc40fa19d17fc084e966ed97cde4ace
jmspa3 <57222312+jmspa3@users.noreply.github.com>
2021-02-13 22:32:36 +0000

    Update raspage.blade.php

--- IEEE/resources/views/societies/raspage.blade.php
+++ IEEE/resources/views/societies/raspage.blade.php
@@ -74,14 +74,217 @@
 <section id="section2" class="py-5 section-2"><h2 class="letter2 sections-title" >Workshops</h2>
     <div class="container py-5 text-center">
 
-        <div class="row">
-            <div class="col-lg-8 mx-auto">
-           
-                <p class="text-muted lead sections-description" >Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.</p>
+    <div class="event-slider owl-carousel">
+                <div class="event-card mx-auto">
+                    <img class="event-card-img-top event-img" src="{{asset('img/ieee1.jpg')}}">
+                    <div class="card-ellipse">
+                    </div>
+                    <div class="event-card-body">
+                        <div class="event-card-body-title">
+                            Event1
+                        </div>
+                        <div class="event-card-body-caption">
+                            Event1 caption
+                        </div>
+                        <div class="event-card-body-seemore">
+                            See more ⇀
+                        </div>
+                    </div>
+                </div>
+                <div class="event-card mx-auto">
+                    <img class="event-card-img-top event-img" src="{{asset('img/ieee2.jpg')}}">
+                    <div class="card-ellipse">
+                    </div>
+                    <div class="event-card-body">
+                        <div class="event-card-body-title">
+                            Event2
+                        </div>
+                        <div class="event-card-body-caption">
+                            Event2 caption
+                        </div>
+                        <div class="event-card-body-seemore">
+                            See more ⇀
+                        </div>
+                    </div>
+                </div>
+                <div class="event-card mx-auto">
+                    <img class="event-card-img-top event-img" src="{{asset('img/ieee3.jpg')}}">
+                    <div class="card-ellipse">
+                    </div>
+                    <div class="event-card-body">
+                        <div class="event-card-body-title">
+                            Event3
+                        </div>
+                        <div class="event-card-body-caption">
+                            Event3
+                        </div>
+                        <div class="event-card-body-seemore">
+                            See more ⇀
+                        </div>
+                    </div>
+                </div>
+                <div class="event-card mx-auto">
+                    <img class="event-card-img-top event-img" src="{{asset('img/ieee2.jpg')}}">
+                    <div class="card-ellipse">
+                    </div>
+                    <div class="event-card-body">
+                        <div class="event-card-body-title">
+                            Event4
+                        </div>
+                        <div class="event-card-body-caption">
+                            Event4 caption
+                        </div>
+                        <div class="event-card-body-seemore">
+                            See more ⇀
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
+
+<style>
+.event-card {
+                overflow: hidden;
+                height:30rem;
+                transition:0.6s;
+                border-radius:1px;
+                position:relative;
+                display:block;
+                outline: none;
+                margin-bottom: 16%;
+                margin-top: 16%;
+                -webkit-box-shadow: 0px 0px 15px 5px rgba(0,0,0,0.2);
+                box-shadow: 0px 0px 15px 5px rgba(0,0,0,0.2);
+                width:90%;
+             }
+
+            .event-card:hover {
+                -webkit-box-shadow: 0px 0px 15px 5px rgba(0,0,0,0.4);
+                box-shadow: 0px 0px 15px 5px rgba(0,0,0,0.4);
+            }
+
+            .event-card:hover > .event-card-body{
+                transition: .3s;
+                height:45%;
+            }
+
+            .event-card:hover > .card-ellipse{
+                transition: .3s;
+                height:53%;
+            }
+
+            .event-card:hover > .event-img{
+                transition: .3s;
+                transform:translateY(-30px);
+                transform:scale(1.1, 1.1);
+                filter:brightness(50%);
+            }
+
+            .event-card:hover > .event-card-body > .event-card-body-description{
+                transition: .3s;
+                transform:translateY(-50px);
+                opacity:1;
+            }
+
+            .card-ellipse {
+                position: absolute;
+                transition: .3s;
+                background-color: white;
+                clip-path: ellipse(90% 56% at 49% 57%);
+                height: 45%;
+                width: 100%;
+                bottom: 0;
+                -webkit-box-shadow: -10px 0 30px 5px #222;
+                box-shadow: -10px 0 30px 5px #222;
+            }
+
+            .event-card:hover > .event-card-body > .event-card-body-caption{
+                transition: .3s;
+                transform:translateY(-0.5rem);
+                opacity:1;
+            }
+
+            .event-card:hover > .event-card-body > .event-card-body-seemore{
+                transition: .3s;
+                padding-right: 3%;
+                opacity:1;
+            }
+
+            .event-img {
+                object-fit: cover;
+                transition: .3s;
+                height:65%;
+            }
+
+            .event-card-body {
+                background:transparent;
+                color: black;
+                transition: .3s;
+                height:35%;
+                width:95%;
+                padding-left:2rem;
+                position:absolute;
+                bottom:0;
+            }
+
+            .event-card-body-title {
+                text-align: left;
+                font-size: 40px;
+                font-weight: bold;
+            }
+
+            .event-card-body-caption {
+                text-align: left;
+                font-size: 25px;
+                padding-bottom: 0.5rem;
+                padding-top: 1.5rem;
+                opacity:0;
+                transition: .3s;
+                -webkit-line-clamp: 2;
+                text-overflow: ellipsis;
+                overflow : hidden;
+                display: -webkit-box;
+                -webkit-box-orient: vertical;
+            }
+
+            .event-card-body-seemore {
+                text-align: right;
+                font-size: 18px;
+                margin-bottom: 1rem;
+                padding-top: 2rem;
+                border-bottom: 1px solid black;
+                padding-bottom: 1px;
+                opacity:0;
+                transition: .3s;
+                -webkit-line-clamp: 2;
+                overflow : hidden;
+                text-overflow: ellipsis;
+                display: -webkit-box;
+                -webkit-box-orient: vertical;
+                position: absolute;
+                bottom: 0;
+                right: 0;
+            }
+
+           .owl-prev, .owl-next{
+               position: absolute !important;
+               outline:none !important;
+           }
+
+            .owl-prev{
+               top: 40%;
+               outline:none !important;
+               left: -30px;
+            }
+
+            .owl-next{
+               top: 40%;
+               outline:none !important;
+               right: -30px;
+            }
+            </style>
     </div>
 </section>
+
 <section id="section2" class="py-5 section-2"><h2 class="letter2 sections-title">Repositório de ideias</h2>
     <div class="container py-5 text-center">
 
